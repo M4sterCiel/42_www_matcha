bee1942f344a5f69d2a13ef0a626d36347a9b81d
Lucas CORDENOD <lcordeno@e3r10p14.42.fr>
2019-06-04 18:26:19 +0200

    Started login front end validation

--- front/src/Login.js
+++ front/src/Login.js
@@ -1,33 +1,95 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
 //import Footer from './components/Footer';
-import 'materialize-css/dist/css/materialize.min.css';
+import "materialize-css/dist/css/materialize.min.css";
 
 class Login extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { 
-      login: '',
-      pwd: '',
-      responseToPost: '' };
+    this.state = {
+      login: "",
+      pwd: "",
+      loginError: "",
+      pwdError: "",
+      responseToPost: ""
+    };
   }
+
+  validateName = () => {
+    let loginError = "";
+    let regexName = /^[a-zA-Z0-9_.-]*$/;
+    let regexEmail = /^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,})$/;
+
+    if (
+      !this.state.login.match(regexName) &&
+      !this.state.login.match(regexEmail)
+    ) {
+      loginError = "Invalid login";
+    }
+
+    if (loginError) {
+      this.setState({ loginError });
+      return false;
+    }
+
+    this.setState({ loginError });
+    return true;
+  };
+
+  validatePwd = () => {
+    let pwdError = "";
+
+    if (this.state.pwd.includes("2")) {
+      pwdError = "Invalid password";
+    }
+
+    if (pwdError) {
+      this.setState({ pwdError });
+      return false;
+    }
+
+    this.setState({ pwdError });
+    return true;
+  };
+
+  handleChange = e => {
+    const isLogin = e.target.name === "name";
+    const isPwd = e.target.name === "pwd";
+
+    if (isLogin) {
+      this.setState({ login: e.target.value });
+      const isValidLogin = this.validateName();
+
+      if (isValidLogin) {
+        console.log(this.state);
+      }
+    }
+
+    if (isPwd) {
+      this.setState({ pwd: e.target.value });
+      const isValidPwd = this.validatePwd();
+
+      if (isValidPwd) {
+        console.log(this.state);
+      }
+    }
+  };
 
   handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('/users/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', },
-      body: JSON.stringify({ 
+    const response = await fetch("/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
         login: this.state.login,
         pwd: this.state.pwd
-      }), 
+      })
     });
     const body = await response.text();
-    this.setState({ responseToPost: body.status});
+    this.setState({ responseToPost: body.status });
     console.log(body);
-  }
+  };
   /* componentDidMount() {
     
     this.callApi()
@@ -44,34 +106,54 @@ class Login extends Component {
       return body;
   }; */
 
-    render() {
-      return (
-        <div className="App">
-          <NavBar />
-          <div className="row">
-            <div className="col a12 m6" id="login-box">
-              <div className="card-panel center">
-                <i className="medium material-icons">account_box</i>
-                <div className="card-panel">
-                  <form onSubmit={this.handleSubmit}>
-                    <div className="input-field">
-                      <input type="text" name="name" id="user-login" value={this.state.login} onChange={e => this.setState({ login: e.target.value })} required ></input>
-                      <label htmlFor="user-login">Username or email</label>
-                    </div>
-                    <div className="input-field">
-                      <input type="password" name="pwd" id="pwd-login" value={this.state.pwd} onChange={e => this.setState({ pwd: e.target.value })} required></input>
-                      <label htmlFor="pwd-login">Password</label>
-                    </div>
-                    <input type="submit" name="submit" value="Se connecter" className="btn"></input>
-                  </form>
-                </div>
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <div className="row">
+          <div className="col a12 m6" id="login-box">
+            <div className="card-panel center">
+              <i className="medium material-icons">account_box</i>
+              <div className="card-panel">
+                <form onSubmit={this.handleSubmit}>
+                  <div className="input-field">
+                    <input
+                      type="text"
+                      name="name"
+                      id="user-login"
+                      value={this.state.login}
+                      onChange={this.handleChange}
+                      required
+                    />
+                    <div className="login-error">{this.state.loginError}</div>
+                    <label htmlFor="user-login">Username or email</label>
+                  </div>
+                  <div className="input-field">
+                    <input
+                      type="password"
+                      name="pwd"
+                      id="pwd-login"
+                      value={this.state.pwd}
+                      onChange={this.handleChange}
+                      required
+                    />
+                    <div className="pwd-error">{this.state.pwdError}</div>
+                    <label htmlFor="pwd-login">Password</label>
+                  </div>
+                  <input
+                    type="submit"
+                    name="submit"
+                    value="Se connecter"
+                    className="btn"
+                  />
+                </form>
               </div>
             </div>
           </div>
         </div>
-      );
-    }
-
+      </div>
+    );
+  }
 }
 
 export default Login;
