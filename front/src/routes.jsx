8ff2717c511b36c5ff7b45272feb7d0a768429e0
Lucas CORDENOD <lcordeno@e3r12p12.42.fr>
2019-06-10 18:54:55 +0200

    Updated front end style and links in header

--- front/src/routes.jsx
+++ front/src/routes.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
- 
-import App from './App'
-import Login from './Login'
-import Register from "./Register"
- 
-export default function MainRouter () {
-    return (
-        <Router>
-            <div>
-                <Route exact path="/" component={App}/>
-                <Route path="/users/login" component={Login}/>
-                <Route path="/users/register" component={Register}/>
-            </div>
-        </Router>
-    )
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import App from "./App";
+import Login from "./Login";
+import Register from "./Register";
+
+export default function MainRouter() {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/users/login" component={Login} />
+        <Route path="/users/register" component={Register} />
+      </div>
+    </Router>
+  );
 }
