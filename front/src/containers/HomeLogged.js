a51ce186afa0a795e559f31c77df4d172961c2c6
Lucas CORDENOD <lucas.cordenod@gmail.com>
2019-08-21 20:00:47 +0200

    Started working on user cards

--- front/src/containers/HomeLogged.js
+++ front/src/containers/HomeLogged.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import UserCard from "../components/cards/UserCard";
 import CompleteProfile from "../components/home/CompleteProfile";
 import ModalUserEditProfileInfo from "../components/modals/ModalUserEditProfileInfo";
 import ModalUserEditProfilePictures from "../components/modals/ModalUserEditProfilePictures";
@@ -10,31 +11,36 @@ class HomeLogged extends Component {
       <div className="App">
         <div className="row">
           {this.props.userConnectedData.id !== undefined &&
-            (this.props.userConnectedData.gender === null ||
-              this.props.userConnectedData.sexual_orientation === null ||
-              this.props.userConnectedData.bio === null ||
-              this.props.userConnectedData.birthdate === null ||
-              this.props.userConnectedData.tags.length === 0 ||
-              this.props.userConnectedData.pictures.length === 0) && (
-              <div>
-                <CompleteProfile
-                  infoEdit={
-                    this.props.userConnectedData.gender === null ||
-                    this.props.userConnectedData.sexual_orientation === null ||
-                    this.props.userConnectedData.bio === null ||
-                    this.props.userConnectedData.birthdate === null ||
-                    this.props.userConnectedData.tags.length === 0
-                  }
-                  picEdit={this.props.userConnectedData.pictures.length === 0}
-                />
-                {this.props.userConnectedData.id !== undefined && (
-                  <div>
-                    <ModalUserEditProfileInfo />
-                    <ModalUserEditProfilePictures />
-                  </div>
-                )}
-              </div>
-            )}
+          (this.props.userConnectedData.gender === null ||
+            this.props.userConnectedData.sexual_orientation === null ||
+            this.props.userConnectedData.bio === null ||
+            this.props.userConnectedData.birthdate === null ||
+            this.props.userConnectedData.tags.length === 0 ||
+            this.props.userConnectedData.pictures.length === 0) ? (
+            <div>
+              <CompleteProfile
+                infoEdit={
+                  this.props.userConnectedData.gender === null ||
+                  this.props.userConnectedData.sexual_orientation === null ||
+                  this.props.userConnectedData.bio === null ||
+                  this.props.userConnectedData.birthdate === null ||
+                  this.props.userConnectedData.tags.length === 0
+                }
+                picEdit={this.props.userConnectedData.pictures.length === 0}
+              />
+              {this.props.userConnectedData.id !== undefined && (
+                <div>
+                  <ModalUserEditProfileInfo />
+                  <ModalUserEditProfilePictures />
+                </div>
+              )}
+            </div>
+          ) : (
+            <div>
+              <UserCard />
+              <UserCard />
+            </div>
+          )}
         </div>
       </div>
     );
