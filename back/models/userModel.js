53ee0cceb5871f40ffe9162031ccd699baf3e127
raphael <raphael.allemand@live.fr>
2019-08-29 10:43:04 +0200

    adding seed function

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -84,7 +84,7 @@ module.exports = {
     try {
       var result = await pool.query({
         sql:
-          "INSERT INTO users (lastname, firstname, username, gender, sexual_orientation, mail, bio, birthdate, password, city, geo_lat, geo_long) VALUES (?)",
+          "INSERT INTO users (lastname, firstname, username, gender, sexual_orientation, mail, bio, birthdate, password, city, status) VALUES (?)",
         values: [data]
       });
       return result.insertId;
