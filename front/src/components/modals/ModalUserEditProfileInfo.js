e91cacd970737276f034041ca81befd309ad5537
raphael <raphael.allemand@live.fr>
2019-09-09 10:11:52 +0200

    fixing birthdate checking input

--- front/src/components/modals/ModalUserEditProfileInfo.js
+++ front/src/components/modals/ModalUserEditProfileInfo.js
@@ -129,7 +129,7 @@ class ModalUserEditProfileInfo extends Component {
         lastname: this.state.lastname,
         bio: this.state.bio === "" ? null : this.state.bio,
         birthdate:
-          this.state.birthdate === null
+          this.state.birthdate === null || this.state.birthdate === ""
             ? null
             : moment(new Date(this.state.birthdate))
                 .format()
