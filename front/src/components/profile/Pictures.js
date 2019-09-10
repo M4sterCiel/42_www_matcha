dea0453741d950dc56a7cb3ecdcd7894e2fd0181
Lucas CORDENOD <lucas.cordenod@gmail.com>
2019-09-10 12:25:13 +0200

    Removed commented code

--- front/src/components/profile/Pictures.js
+++ front/src/components/profile/Pictures.js
@@ -1,34 +1,9 @@
 import React, { Component } from "react";
-/* import { Slider, Slide } from "react-materialize"; */
 import Slider from "react-slick";
 import NoImage from "../../assets/no-image.jpeg";
 
 class Pictures extends Component {
   render() {
-    /*     const NoSlide = (
-      <Slide image={<img src={NoImage} alt="no pic set" />}></Slide>
-    );
-
-    const Slides = this.props.pictures.map(picture => (
-      <Slide
-        key={picture.id}
-        image={<img src={picture.url} alt={"picture " + picture.index} />}
-      />
-    )); */
-
-    /*     return (
-      <div className="card">
-        <div className="card-content">
-          <p className="profile-info-title">Pictures</p>
-          {this.props.pictures !== undefined && (
-            <Slider>
-              {this.props.pictures.length === 0 ? NoSlide : Slides}
-            </Slider>
-          )}
-        </div>
-      </div>
-    ); */
-
     const NoSlide = (
       <div className="slide">
         <img className="slide-img" src={NoImage} alt="no pic set" />
